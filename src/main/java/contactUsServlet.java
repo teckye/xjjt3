3c02777cabcf169c111a60cbca3ca0c8f2561463
Chew Yi <Chew Yi@DESKTOP-JVMQGJU>
2023-02-03 15:13:19 +0800

    UPDATE contact us imported dependecies

--- src/main/java/contactUsServlet.java
+++ src/main/java/contactUsServlet.java
@@ -1,13 +1,16 @@
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
 /**
  * Servlet implementation class contactUsServlet
  */
